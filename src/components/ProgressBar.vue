946bc951c668fc82d6179ff85d79137f8c0220d9
Nícolas Carvalho <nicolassprofissional@gmail.com>
2024-06-02 19:17:39 -0300

    :sparkles: feat: pokemon progress bar component

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+
+    import {ref, onBeforeMount} from 'vue'
+
+    const props = defineProps<{
+        title: string,
+        percentage: number
+    }>()
+
+    const percentageString = ref(String(props.percentage))
+    
+</script>
+
+
+<template>  
+    <div class="progress-bar">
+        <label><p>{{props.title}}: {{props.percentage}}%</p></label>
+        <progress :value="percentageString" max="100" /> 
+    </div>
+</template>
+
+
+<style scoped>
+
+    .progress-bar{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .progress-bar p{
+        margin-right: 20px;
+    }
+
+    .progress-bar progress {
+        height: 20px;
+        -webkit-appearance: none;
+    }
+
+    .progress-bar progress::-webkit-progress-bar {
+        background-color: white;
+        border-radius: 20px;
+        box-shadow: 0px 1px 4px 0px #333333;
+    }
+
+    .progress-bar progress::-webkit-progress-value {
+        background-color: #333333;
+        border-radius: 20px;
+    }
+</style>
