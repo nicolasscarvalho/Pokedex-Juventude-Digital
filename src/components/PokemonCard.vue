40cea51f75f859f9a21e5f80f1dcca596f56ce0d
Nícolas Carvalho <nicolassprofissional@gmail.com>
2024-06-02 19:16:24 -0300

    :sparkles: feat: pokemon card component

--- src/components/PokemonCard.vue
+++ src/components/PokemonCard.vue
@@ -4,23 +4,30 @@
         pokemonIndex: number,
         pokemonImg: string,
         pokemonName: string,
-        pokemonType: string
+        pokemonType: string[]
     }>()
 
-    const title = `${props.pokemonIndex}. ${props.pokemonName}`
+    const title = `${props.pokemonIndex + 1}. ${props.pokemonName}`
+    const types = props.pokemonType.join(' / ')
+    const detailRoute = `/pokemon-detail/${props.pokemonName}`
 
 </script>
 
+
 <template>
-    <div class="pokemon-card">
-        <img src="#" placeholder="Imagem do Pokemon">
-        <section>
-            <h2>{{ title }}</h2>
-            <p>{{ props.pokemonType }}</p>
-        </section>
-    </div>
+
+    <RouterLink :to="{name: 'PokemonDetail', params: {pokemonName: props.pokemonName}}" class="router-link">
+        <div class="pokemon-card">
+            <img :src="props.pokemonImg" placeholder="Imagem do Pokemon">
+            <section>
+                <h2>{{ title }}</h2>
+                <p>{{ types }}</p>
+            </section>
+        </div>
+    </RouterLink>
 
 </template>
+
 
 <style scoped>
     .pokemon-card {
@@ -31,13 +38,15 @@
         width: 406px;
         height: 150px;
         box-shadow: 0px 8px 22px 6px #18274B1F;
-        transition: 250ms;
         display: flex;
         align-items: center;
         justify-content: space-around;
         cursor: pointer;
     }
 
+    .router-link{
+        text-decoration: none;
+    }
 
     .pokemon-card:hover {
         background-color: #333333;
