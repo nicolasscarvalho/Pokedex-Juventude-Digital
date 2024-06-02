032aab4d78efbe4cb726d558e5a5013c8c1a835f
Nícolas Carvalho <nicolassprofissional@gmail.com>
2024-06-02 19:18:59 -0300

    :sparkles: feat: pokemon type

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -1,8 +1,13 @@
 export class Pokemon {
     constructor(
-        public pokemonIndex: number,
         public pokemonImg: string,
         public pokemonName: string,
-        public pokemonType: string
+        public pokemonType: string[],
+        public pokemonHeight: number,
+        public pokemonWeight: number, 
+        public pokemonHP: number,
+        public pokemonAttack: number, 
+        public pokemonDefense: number,
+        public pokemonSpeed: number
     ){}
 }
